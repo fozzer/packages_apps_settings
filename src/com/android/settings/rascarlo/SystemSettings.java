2eb9c4e3eac1bf8578f890656c1fb775149b3404
fozzil4 <roadk1ll@hotmail.com>
2014-05-09 01:03:50 -0500

    Revert "don't show status bar preference screen on tablet"
    This reverts commit a3d09767560d9c4f927bdc32999d1fcddd16162b.

--- src/com/android/settings/rascarlo/SystemSettings.java
+++ src/com/android/settings/rascarlo/SystemSettings.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2012 The CyanogenMod project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2012 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.android.settings.rascarlo;
 
@@ -23,15 +23,12 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 public class SystemSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "SystemSettings";
-    private static final String KEY_STATUS_BAR = "status_bar";
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
 
-    private PreferenceScreen mStatusBar;
     private PreferenceScreen mNotificationPulse;
 
     @Override
@@ -40,20 +37,11 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.system_settings);
 
-        // don't show status bar preference screen on tablet
-        // available settings right now are for phones only
-        mStatusBar = (PreferenceScreen) findPreference(KEY_STATUS_BAR);
-            if (!Utils.isPhone(getActivity())) {
-                if (mStatusBar != null) {
-                getPreferenceScreen().removePreference(mStatusBar);
-            }
-        }
-
         // Led settings
         mNotificationPulse = (PreferenceScreen) findPreference(KEY_NOTIFICATION_PULSE);
         if (mNotificationPulse != null) {
             if (!getResources().getBoolean(
-                    com.android.internal.R.bool.config_intrusiveNotificationLed)) {
+                com.android.internal.R.bool.config_intrusiveNotificationLed)) {
                 getPreferenceScreen().removePreference(mNotificationPulse);
             } else {
                 updateLightPulseDescription();
