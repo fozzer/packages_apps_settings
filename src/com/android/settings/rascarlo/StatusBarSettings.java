400ac9f35ee5513eec81f07d237937173d491568
fozzil4 <roadk1ll@hotmail.com>
2014-05-08 23:51:43 -0500

    Status bar: double-tap to sleep
    Conflicts:
    	res/values/ras_strings.xml
    	res/xml/status_bar_settings.xml
    	src/com/android/settings/rascarlo/StatusBarSettings.java

--- src/com/android/settings/rascarlo/StatusBarSettings.java
+++ src/com/android/settings/rascarlo/StatusBarSettings.java
@@ -1,21 +1,34 @@
-
 package com.android.settings.rascarlo;
 
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class StatusBarSettings extends SettingsPreferenceFragment implements
-OnPreferenceChangeListener {
+public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    // General
+    private static String STATUS_BAR_GENERAL_CATEGORY = "status_bar_general_category";
+    // Double-tap to sleep
+    private static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+    // Quick Settings
+    private static final String QUICK_SETTINGS_CATEGORY = "status_bar_quick_settings_category";
     private static final String QUICK_PULLDOWN = "quick_pulldown";
 
+    // General
+    private PreferenceCategory mStatusBarGeneralCategory;
+    // Double-tap to sleep
+    private CheckBoxPreference mStatusBarDoubleTapSleepGesture;
+    // Quick Settings
     private ListPreference mQuickPulldown;
 
     @Override
@@ -24,34 +37,51 @@ OnPreferenceChangeListener {
 
         addPreferencesFromResource(R.xml.status_bar_settings);
 
-        // Quick Settings pull down
-        mQuickPulldown = (ListPreference) getPreferenceScreen().findPreference(QUICK_PULLDOWN);
-        mQuickPulldown.setOnPreferenceChangeListener(this);
-        int quickPulldownValue = Settings.System.getInt(getActivity().getApplicationContext()
-                .getContentResolver(),
-                Settings.System.QS_QUICK_PULLDOWN, 0);
-        mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
-        updatePulldownSummary(quickPulldownValue);
-    }
+            // General category
+            mStatusBarGeneralCategory = (PreferenceCategory) findPreference(STATUS_BAR_GENERAL_CATEGORY);
 
-    private void updatePulldownSummary(int value) {
-        Resources res = getResources();
-        if (value == 0) {
-            /* quick pulldown deactivated */
-            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
-        } else {
-            String direction = res.getString(value == 2
-                    ? R.string.quick_pulldown_summary_left : R.string.quick_pulldown_summary_right);
-            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
+            // Status bar double-tap to sleep
+            mStatusBarDoubleTapSleepGesture = (CheckBoxPreference) getPreferenceScreen().findPreference(DOUBLE_TAP_SLEEP_GESTURE);
+            mStatusBarDoubleTapSleepGesture.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1));
+
+            // Quick settings category
+            // Quick Settings pull down
+            mQuickPulldown = (ListPreference) getPreferenceScreen().findPreference(QUICK_PULLDOWN);
+            // only show on phones
+            if (!Utils.isPhone(getActivity())) {
+                if (mQuickPulldown != null)
+                    getPreferenceScreen().removePreference(mQuickPulldown);
+                getPreferenceScreen().removePreference((PreferenceCategory) findPreference(QUICK_SETTINGS_CATEGORY));
+            } else {
+                mQuickPulldown.setOnPreferenceChangeListener(this);
+                int quickPulldownValue = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                        Settings.System.QS_QUICK_PULLDOWN, 0);
+                mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
+                mQuickPulldown.setSummary(mQuickPulldown.getEntry());
+            }
         }
-    }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+
         if (preference == mQuickPulldown) {
             int quickPulldownValue = Integer.valueOf((String) objValue);
+            int quickPulldownIndex = mQuickPulldown.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.QS_QUICK_PULLDOWN, quickPulldownValue);
-            updatePulldownSummary(quickPulldownValue);
+            mQuickPulldown.setSummary(mQuickPulldown.getEntries()[quickPulldownIndex]);
+            return true;
+
+        }
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+        if (preference == mStatusBarDoubleTapSleepGesture) {
+            value = mStatusBarDoubleTapSleepGesture.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE, value ? 1: 0);
             return true;
         }
         return false;
