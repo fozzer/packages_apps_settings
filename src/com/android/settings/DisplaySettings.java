d9d26f8ae189936ec93c9360ba74784449cffeb7
fozzil4 <roadk1ll@hotmail.com>
2014-05-09 23:10:21 -0500

    Bring back audio features & other stuff
    - Added basic volume features
    - added led control category
    - added system_setting icons by Ruse Ciprian
    
    Status bar brightness control
    
    fix derp
    
    Conflicts:
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -56,6 +56,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
     private static final String KEY_SCREEN_SAVER = "screensaver";
+    private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -64,6 +65,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private PreferenceCategory mLightOptions;
     private PreferenceScreen mNotificationPulse;
     private PreferenceScreen mBatteryPulse;
+    private CheckBoxPreference mVolumeWake;
 
     private final Configuration mCurConfig = new Configuration();
     
@@ -145,6 +147,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             } else {
                 updateBatteryPulseDescription();
             }
+        }
+
+        mVolumeWake = (CheckBoxPreference) findPreference(KEY_VOLUME_WAKE);
+        if (mVolumeWake != null) {
+            mVolumeWake.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
+            mVolumeWake.setOnPreferenceChangeListener(this);
         }
     }
 
@@ -309,18 +318,18 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private void updateLightPulseDescription() {
         if (Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_PULSE, 0) == 1) {
-            mNotificationPulse.setSummary(getString(R.string.notification_light_enabled));
+            mNotificationPulse.setSummary(getString(R.string.enabled));
         } else {
-            mNotificationPulse.setSummary(getString(R.string.notification_light_disabled));
+            mNotificationPulse.setSummary(getString(R.string.disabled));
          }
     }
 
     private void updateBatteryPulseDescription() {
         if (Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.BATTERY_LIGHT_ENABLED, 1) == 1) {
-            mBatteryPulse.setSummary(getString(R.string.notification_light_enabled));
+            mBatteryPulse.setSummary(getString(R.string.enabled));
         } else {
-            mBatteryPulse.setSummary(getString(R.string.notification_light_disabled));
+            mBatteryPulse.setSummary(getString(R.string.disabled));
         }
      }
 
@@ -348,7 +357,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (KEY_FONT_SIZE.equals(key)) {
             writeFontSizePreference(objValue);
         }
-
+        if (KEY_VOLUME_WAKE.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_WAKE_SCREEN,
+                    (Boolean) objValue ? 1 : 0);
+        }
         return true;
     }
 
